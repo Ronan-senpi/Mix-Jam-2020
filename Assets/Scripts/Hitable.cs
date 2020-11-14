897c114cc2a9d15b8a7ca89e9cfeae121d43b899
Ronan <30617930+MWRonanD@users.noreply.github.com>
2020-11-14 01:17:27 +0100

    Shoot - Kill - Grab

---
+++ Assets/Scripts/Hitable.cs
@@ -0,0 +1,42 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitable : MonoBehaviour
+{
+    [SerializeField]
+    private LayerMask hitableMask;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((hitableMask.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            Projectil projectil;
+            if (collision.transform.TryGetComponent<Projectil>(out projectil))
+            {
+                switch (projectil.GetProjectilType())
+                {
+                    case ProjectilType.Grab:
+                        Grab();
+                        break;
+                    case ProjectilType.Kill:
+                        Kill();
+                        break;
+                }
+            }
+        }
+    }
+
+    private void Kill()
+    {
+        Destroy(gameObject);
+    }
+    private void Grab()
+    {
+        Rigidbody2D rb;
+        if (transform.TryGetComponent(out rb))
+        {
+            rb.AddForce(Vector2.one);
+        }
+    }
+}
---
+++ Assets/Scripts/Projectils/Projectil.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectil : MonoBehaviour
+{
+    [SerializeField]
+    float speed;
+    [SerializeField]
+    float lifeTime;
+    float lifepass = 0;
+    [SerializeField]
+    ProjectilType projectilType;
+
+    public ProjectilType GetProjectilType()
+    {
+        return projectilType;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localPosition += Vector3.up * Time.deltaTime * speed;
+        lifepass += Time.deltaTime;
+        if (lifepass >= lifeTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        this.HitSomthing();
+    }
+
+    private void HitSomthing()
+    {
+        Destroy(gameObject);
+    }
+}
