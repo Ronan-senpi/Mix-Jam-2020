3a556ee05ab51c236c27d67b71079d402c27c0b2
Ronan <30617930+MWRonanD@users.noreply.github.com>
2020-11-14 21:38:59 +0100

    Food assez + shooting react

---
+++ Assets/Scripts/Food/TakeFood.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeFood : MonoBehaviour
+{
+    [SerializeField]
+    LayerMask hitMask;
+
+    List<GameObject> storageFood = new List<GameObject>();
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if ((hitMask.value & (1 << collision.gameObject.layer)) > 0)
+        {
+            Food food;
+            if (collision.transform.TryGetComponent(out food))
+            {
+                this.storageFood.Add(collision.transform.gameObject);
+                Destroy(collision.transform.gameObject);
+            }
+        }
+    }
+}
