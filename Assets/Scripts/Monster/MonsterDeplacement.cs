d3a25e23e7f8808c881a1a17935aabd6e87fb0fc
farcy <farcyalexandre.60@hotmail.fr>
2020-11-15 00:21:27 +0100

    scene 1 pour ronan

---
+++ Assets/Scripts/Monster/MonsterDeplacement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterDeplacement : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+
+    private Transform targetFood;
+    private Transform targetPlayer; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector2.Distance(transform.position,targetPlayer.position)>stoppingDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+        }
+        
+    }
+}
