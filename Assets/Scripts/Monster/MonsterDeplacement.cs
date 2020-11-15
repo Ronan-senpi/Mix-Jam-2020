d19629b46c3f4898465d6b3ef6df961dedad1359
farcy <farcyalexandre.60@hotmail.fr>
2020-11-15 16:15:34 +0100

    Upgrade Monster + look at monster + spawn food

--- Assets/Scripts/Monster/MonsterDeplacement.cs
+++ Assets/Scripts/Monster/MonsterDeplacement.cs
@@ -12,12 +12,15 @@ public class MonsterDeplacement : MonoBehaviour
 
     public bool food;
 
-    public int state = 1; 
+    public int state = 1;
+
+    private Rigidbody2D rb;
 
     // Start is called before the first frame update
     void Start()
     {
         targetPlayer = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        rb = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -28,6 +31,10 @@ public class MonsterDeplacement : MonoBehaviour
             if (Vector2.Distance(transform.position, targetPlayer.position) > stoppingDistance)
             {
                 transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+                Vector3 direction = targetPlayer.position - transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+                rb.rotation = angle;
+
                 GameObject food = GameObject.FindGameObjectWithTag("Food");
                 if(food !=null)
                 {
@@ -41,10 +48,14 @@ public class MonsterDeplacement : MonoBehaviour
             if(Vector2.Distance(transform.position,targetFood.position)< 5)
             {
                 transform.position = Vector2.MoveTowards(transform.position, targetFood.position, speed * Time.deltaTime);
+                Vector3 direction = targetFood.position - transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             }
             else
             {
                 transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+                Vector3 direction = targetPlayer.position - transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             }
         }
         else if(state >=3)
@@ -52,11 +63,40 @@ public class MonsterDeplacement : MonoBehaviour
             if (Vector2.Distance(transform.position, targetPlayer.position) > stoppingDistance)
             {
                 transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+                Vector3 direction = targetPlayer.position - transform.position;
+                float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
             }
         }
 
-        
-        
-        
+        if(state == 1)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (state == 2)
+        {
+            transform.localScale = new Vector3(1.25f, 1.25f, 1.25f);
+
+        }
+        else if (state == 3)
+        {
+            transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+
+        }
+
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.collider.gameObject.layer == LayerMask.NameToLayer("Projectils"))
+        {
+            state = state - 1;
+            if (state <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+
+        }
     }
 }
