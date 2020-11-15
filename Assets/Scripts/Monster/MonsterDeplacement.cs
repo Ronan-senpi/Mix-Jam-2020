0a82bcdb695ca7f5d886c7605589f26e854aea0d
farcy <farcyalexandre.60@hotmail.fr>
2020-11-15 02:08:44 +0100

    ajout des vers

--- Assets/Scripts/Monster/MonsterDeplacement.cs
+++ Assets/Scripts/Monster/MonsterDeplacement.cs
@@ -8,7 +8,11 @@ public class MonsterDeplacement : MonoBehaviour
     public float stoppingDistance;
 
     private Transform targetFood;
-    private Transform targetPlayer; 
+    private Transform targetPlayer;
+
+    public bool food;
+
+    public int state = 1; 
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +23,40 @@ public class MonsterDeplacement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Vector2.Distance(transform.position,targetPlayer.position)>stoppingDistance)
+        if(targetFood == null && state<3)
         {
-            transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+            if (Vector2.Distance(transform.position, targetPlayer.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+                GameObject food = GameObject.FindGameObjectWithTag("Food");
+                if(food !=null)
+                {
+                    targetFood =food.GetComponent<Transform>();
+
+                }
+            }
         }
+        else if (targetFood)
+        {
+            if(Vector2.Distance(transform.position,targetFood.position)< 5)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetFood.position, speed * Time.deltaTime);
+            }
+            else
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+            }
+        }
+        else if(state >=3)
+        {
+            if (Vector2.Distance(transform.position, targetPlayer.position) > stoppingDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, speed * Time.deltaTime);
+            }
+        }
+
+        
+        
         
     }
 }
---
+++ Assets/Scripts/Monster/MonsterFood.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterFood : MonoBehaviour
+{
+    public MonsterDeplacement monsterState;
+    public int monsterIntState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        monsterState =this.gameObject.GetComponent<MonsterDeplacement>();
+        monsterIntState = monsterState.state;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag =="Food")
+        {
+            monsterIntState = monsterIntState + 1;
+            monsterState.state = monsterIntState;
+            Destroy(collision.gameObject);
+        }
+    }
+}
